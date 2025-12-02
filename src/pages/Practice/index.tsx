d7293fb447c4ac445888fe94425a0db140a65428
GowriShankar537797 <shankar537797@gmail.com>
2025-12-02 07:32:01 +0530

    Implement country search with debouncing; update input handling and display results

--- src/pages/Practice/index.tsx
+++ src/pages/Practice/index.tsx
@@ -1,15 +1,27 @@
-import {useState,useEffect} from "react";
+import {useState,useEffect,ChangeEvent} from "react";
 const MyPage=()=>{
     console.log("MyPage");
 const [query,setQuery]=useState("");
 const [countries,setCountries]=useState<string[]>([]);
 
+const fetchCountries=async()=>{
+    const res=await fetch(`https://restcountries.com/v3.1/name/${query}`);
+    const data=await res.json();
+    setCountries(data.map((country:any)=>country));
+}
  useEffect(()=>{
-console.log(query);
+if(!query){
+setCountries([]);
+return;
+}
+   const timeoutId=setTimeout(()=>{
+    fetchCountries();
+   },500);
+   return ()=> clearTimeout(timeoutId);
 
 },[query]);
 
-const handleChange=(e:any)=>{
+const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
 setQuery(e.target.value);
 }
 
@@ -17,7 +29,12 @@ setQuery(e.target.value);
         <div>
             <h1>Debounces</h1>
             <label>Type country</label>
-            <input className="input" type="text" placeholder="type country name" onChange={handleChange}/>
+            <input className="input" type="text" placeholder="type country name" value={query} onChange={handleChange} />
+            {countries.length>0 && (
+                <ul>
+                    {countries.map((country:any,index:number)=> <li key={index}>{country?.name?.common}</li>)}
+                </ul>
+            )}
 
         </div>
     );
